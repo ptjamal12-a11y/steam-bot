647dcf6f46d832da9f0796d310abd31cfd91169f
ptjamal12-a11y <ptjamal12@gmail.com>
2026-06-05 01:20:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import requests
 import re
+import asyncio
 
 TOKEN = "‏8997212415:AAFScTTokC9ugWm3Bu0MDcVW1DJAQaGxmy4"
 
 STEAM_SEARCH = "https://store.steampowered.com/search/?term="
 
+
 def get_image(game):
     try:
-        r = requests.get(STEAM_SEARCH + game.replace(" ", "+"))
+        r = requests.get(STEAM_SEARCH + game.replace(" ", "+"), timeout=10)
         matches = re.findall(r"/app/(\d+)/", r.text)
 
         if matches:
@@ -21,17 +23,24 @@ def get_image(game):
 
     return game, "NOT FOUND"
 
-async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    q = " ".join(context.args)
 
-    if not q:
+async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
         await update.message.reply_text("اكتب: /game اسم اللعبة")
         return
 
+    q = " ".join(context.args)
     title, img = get_image(q)
+
     await update.message.reply_text(f"{title}\n{img}")
 
-app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("game", game))
 
-app.run_polling()
+async def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("game", game))
+
+    await app.run_polling()
+
+
+if _name_ == "_main_":
+    asyncio.run(main())
